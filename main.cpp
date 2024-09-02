1664c2ff50ff6319c5317f78d0241e3c979c7084
github aa4830 <aa4830@naver.com>
2024-09-02 22:16:16 +0900

    chat gpt의 힘을 빌리기

--- main.cpp
+++ main.cpp
@@ -1,30 +1,78 @@
-#include _WINSOCK_DEPRECATED_NO_WARNINGS // 소켓보다 먼저 include해야함
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <WinSock2.h>
 #pragma comment(lib, "ws2_32")
+using namespace std;
 
-int main()
-{
-	WSAData wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
+int main() {
+    WSAData wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cout << "WSAStartup failed with error: " << WSAGetLastError() << endl;
+        return 1;
+    }
 
-	sockaddr_in ServerSocketAddress; // Server주소 구조체 생성
-	memset(&ServerSocketAddress, 0, sizeof(ServerSocketAddress)); //구조체 초기화
+    sockaddr_in ServerSocketAddress;
+    memset(&ServerSocketAddress, 0, sizeof(ServerSocketAddress));
+    ServerSocketAddress.sin_family = AF_INET;
+    ServerSocketAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    ServerSocketAddress.sin_port = htons(10880);
 
-	ServerSocketAddress.sin_family = AF_INET; //구조체 설정1
-	ServerSocketAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); //구조체 설정2
-	ServerSocketAddress.sin_port = htons(10880); //구조체 설정3
+    SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ServerSocket == INVALID_SOCKET) {
+        cout << "Socket creation failed with error: " << WSAGetLastError() << endl;
+        WSACleanup();
+        return 1;
+    }
 
-	SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (connect(ServerSocket, (SOCKADDR*)&ServerSocketAddress, sizeof(ServerSocketAddress)) == SOCKET_ERROR) {
+        cout << "Connection failed with error: " << WSAGetLastError() << endl;
+        closesocket(ServerSocket);
+        WSACleanup();
+        return 1;
+    }
 
-	connect(ServerSocket, (SOCKADDR*)&ServerSocketAddress, sizeof(ServerSocketAddress));
+    // 서버로부터 "10 + 20" 문자열 수신
+    char buffer[1024] = { 0, };
+    int recvBytes = recv(ServerSocket, buffer, sizeof(buffer) - 1, 0); // -1 for null terminator
+    if (recvBytes > 0) {
+        buffer[recvBytes] = '\0'; // Null-terminate the received data
+        printf("Client received: %s\n", buffer);
 
-		char Buffer[1024] = { 0, };
-	int SendLength = send(ServerSocket, Buffer, (int)strlen(Buffer), 0);
+        // 사용자로부터 결과 입력 받기
+        int result;
+        cout << "Enter the result for the expression: ";
+        cin >> result;
 
-	char Message[1024] = { 0, };
-	recv(ServerSocket, Message, sizeof(Message), 0);
+        // 결과를 서버에 전송
+        string resultStr = to_string(result);
+        send(ServerSocket, resultStr.c_str(), (int)resultStr.length(), 0);
 
-	WSACleanup();
+        // 서버로부터 메시지 수신
+        char response[1024] = { 0 };
+        recvBytes = recv(ServerSocket, response, sizeof(response) - 1, 0); // -1 for null terminator
+        if (recvBytes > 0) {
+            response[recvBytes] = '\0'; // Null-terminate the received data
+            printf("Client received message from server: %s\n", response);
+        }
+        else if (recvBytes == 0) {
+            cout << "Connection closed by server." << endl;
+        }
+        else {
+            cout << "recv failed with error: " << WSAGetLastError() << endl;
+        }
+    }
+    else if (recvBytes == 0) {
+        cout << "Connection closed by server." << endl;
+    }
+    else {
+        cout << "recv failed with error: " << WSAGetLastError() << endl;
+    }
 
-	return 0;
+    closesocket(ServerSocket);
+    WSACleanup();
+
+    return 0;
 }
