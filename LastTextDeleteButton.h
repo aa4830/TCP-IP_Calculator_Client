9395f1987d756dd803eee13a6080f7ca51267550
aa4830 <aa4830@naver.com>
2024-09-11 16:31:29 +0900

    모든 글자 초기화 기능 구현

--- LastTextDeleteButton.h
+++ LastTextDeleteButton.h
@@ -18,9 +18,9 @@ public:
     {
         if (MyText.ResultTextSurface != nullptr)
         {
-            SDL_Surface* screenSurface = MyWindow.GetSurface();
-            SDL_FillRect(screenSurface, &MyText.ResultTextPosition, SDL_MapRGB(screenSurface->format, 240, 240, 240));
-            SDL_FreeSurface(MyText.ResultTextSurface);
+            SDL_Surface* ScreenSurface = MyWindow.GetSurface();
+            SDL_FillRect(ScreenSurface, &MyText.ResultTextPosition, SDL_MapRGB(ScreenSurface->format, 240, 240, 240)); //// 텍스트가 그려진 영역을 배경색으로 채워서 지운다
+            SDL_FreeSurface(MyText.ResultTextSurface);  // 기존 텍스트 서페이스를 해제
             MyText.ResultTextSurface = nullptr;
         }
         std::string CurrentText = MyText.CurrentText; 
@@ -30,15 +30,14 @@ public:
         }
         MyText.CurrentText = CurrentText;
 
-        MyText.ResultTextSurface = TTF_RenderText_Solid(MyText.Font, MyText.CurrentText.c_str(), MyText.ResultTextColor);
+        MyText.ResultTextSurface = TTF_RenderText_Solid(MyText.Font, MyText.CurrentText.c_str(), MyText.ResultTextColor);  // 수정된 텍스트로 새로운 서페이스를 생성
         if (MyText.ResultTextSurface == nullptr)
         {
             printf("Font Missing %s\n", TTF_GetError());
         }
         else
         {
-            SDL_BlitSurface(MyText.ResultTextSurface, nullptr, MyWindow.GetSurface(), &MyText.ResultTextPosition);
-            MyWindow.Update();
+            SDL_BlitSurface(MyText.ResultTextSurface, nullptr, MyWindow.GetSurface(), &MyText.ResultTextPosition); // 새 텍스트를 화면에 그린다
         }
     }
 
--- TextClearButton.h
+++ TextClearButton.h
@@ -16,7 +16,26 @@ public:
 
     void HandleLeftClick() override
     {
-        
+        std::cout << "s" << std::endl;
+        if (MyText.ResultTextSurface != nullptr)
+        {
+            SDL_Surface* ScreenSurface = MyWindow.GetSurface();
+            SDL_FillRect(ScreenSurface, &MyText.ResultTextPosition, SDL_MapRGB(ScreenSurface->format, 240, 240, 240)); //// 텍스트가 그려진 영역을 배경색으로 채워서 지운다
+            SDL_FreeSurface(MyText.ResultTextSurface);  // 기존 텍스트 서페이스를 해제
+            MyText.ResultTextSurface = nullptr;
+        }
+
+        MyText.CurrentText.clear();
+
+        MyText.ResultTextSurface = TTF_RenderText_Solid(MyText.Font, MyText.CurrentText.c_str(), MyText.ResultTextColor);  // 수정된 텍스트로 새로운 서페이스를 생성
+        if (MyText.ResultTextSurface == nullptr)
+        {
+            printf("Font Missing %s\n", TTF_GetError());
+        }
+        else
+        {
+            SDL_BlitSurface(MyText.ResultTextSurface, nullptr, MyWindow.GetSurface(), &MyText.ResultTextPosition); // 새 텍스트를 화면에 그린다
+        }
     }
 
 };
